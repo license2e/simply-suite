685d46732796bdc2a91877d5ff88cf233d864add
Ilya Shindyapin <ilya@doublenot.co>
2026-07-09 12:31:23 -0400

    feat: add Tailwind CSS standalone CLI setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.erb",
+    "./app/**/*.rb",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
